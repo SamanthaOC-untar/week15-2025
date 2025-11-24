42b1ddeeae7b8196aa0fea4ab7e238b225809b9f
SamanthaOC-untar <samantha.535240102@stu.untar.ac.id>
2025-11-24 12:53:00 +0700

    PERBARUI DIKIT

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,17 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+const path = require("path");
+
+module.exports = {
+  experimental: {
+    outputFileTracingIncludes: {
+      // sesuaikan dengan route server kamu yang akses db
+      "/api/**": [path.join(__dirname, "prisma/local.db")],
+    },
+  },
+};
+
 export default nextConfig;
+
+
